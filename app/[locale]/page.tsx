6ac87067c01c1a56febafaec2c6fb0465ade512f
Tada <tadasama115599@gmail.com>
2024-07-14 11:36:52 +0700

    update structor home

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,33 +1,136 @@
-'use client'
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { Player, Controls } from '@lottiefiles/react-lottie-player';
-import mainLoad from './../../public/jsons/mainload.json';
+"use client";
+import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { Quicksand, Mitr } from "next/font/google";
+import Link from "next/link";
+import Image from "next/image";
+import initTranslations from "./i18n";
+import TranslationsProvider from "@/components/Provider/TranslationsProvider";
+import MainLoading from "@/components/Loading/MainLoading";
+import styles from "./home/page.module.scss";
 
-export default function Home() {
-  const router = useRouter()
-  const [loading, setLoading] = useState(true);
+const i18nNamespaces = ["homeScreen"];
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+const mitr = Mitr({
+  subsets: ["thai"],
+  weight: ["200", "300", "400", "500", "600", "700"],
+});
+export default function Home({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const [t, setT] = useState<any>(null);
+  const [resources, setResources] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [timeOfDay, setTimeOfDay] = useState<string>("daytime");
+  const currentPathname = usePathname();
+
   useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('/home');
-      setLoading(false);
-    }, 0);
+    const updateTimeOfDay = () => {
+      const hour = new Date().getHours();
+      if (hour >= 6 && hour < 12) {
+        setTimeOfDay("morning");
+      } else if (hour >= 12 && hour < 18) {
+        setTimeOfDay("daytime");
+      } else if (hour >= 18 && hour < 21) {
+        setTimeOfDay("evening");
+      } else {
+        setTimeOfDay("night");
+      }
+    };
+    updateTimeOfDay();
+    const interval = setInterval(updateTimeOfDay, 60000); // Update every minute
+    return () => clearInterval(interval);
+  }, []);
 
-    return () => clearTimeout(timer);
-  }, [router]);
+  useEffect(() => {
+    async function fetchTranslations() {
+      const { t, resources } = await initTranslations(locale, i18nNamespaces);
+      setT(() => t);
+      setResources(resources);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1500);
+    }
+    fetchTranslations();
+  }, [locale]);
 
+  if (loading) {
+    return <MainLoading />;
+  }
 
   return (
-    <div style={{ display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {/* {loading ? (
-        <Player
-          autoplay
-          loop
-          src={mainLoad}
-          style={{ width: '30vh' }}
-        >
-        </Player>
-      ) : null} */}
-    </div>
+    <TranslationsProvider
+      namespaces={i18nNamespaces}
+      locale={locale}
+      resources={resources}
+    >
+      <div className={`${styles.Container} ${styles[timeOfDay]}`}>
+        <div className={styles.MainPannel}>
+          <div className={styles.LeftMediumPannel}>
+            <div className={styles.RowPannel}>
+              <div className={`${styles.Window} ${styles[timeOfDay]}`}>
+                <div className={`${styles.Content} ${styles[timeOfDay]}`}>
+                  <div className={styles.ContentText}>Coming Soon</div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.RowPannel}>
+              <div className={styles.Frame}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+              <div className={styles.Frame}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+              <div className={styles.Frame}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+              <div className={styles.Frame}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+              <div className={styles.Frame}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.SmallPannel}>
+            <div className={styles.ColumnPannel}>
+              <div className={styles.LargeFrame}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+              <div className={styles.LargeFrame}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+              <div className={styles.LargeFrame}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.LargePannel}>
+            <div className={`${styles.RowPannel} ${styles.FullRowPannel}`}>
+              <div className={`${styles.LargeWindow} ${styles[timeOfDay]}`}>
+                <div className={`${styles.Content} ${styles[timeOfDay]}`}></div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.RightMediumPannel}>
+            <div className={styles.RowPannel}>
+              <div className={styles.LargeShelf}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+            </div>
+            <div className={styles.RowPannel}>
+              <div className={styles.LargeShelf}>
+                <div className={styles.Content}>Coming Soon</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </TranslationsProvider>
   );
 }
