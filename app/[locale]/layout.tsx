71d1216af2247a84b9530836ce827a2b0d4d63fa
Dimension Room <51110697+DimensionRoom@users.noreply.github.com>
2025-05-15 11:48:22 +0700

    Convert CSS to SCSS

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,10 +2,9 @@ import React, { ReactNode } from 'react';
 import type { Metadata } from "next";
 import { Quicksand, DotGothic16 } from "next/font/google";
 import { dir } from 'i18next';
-import { ToastProvider , useToast} from '@/context/ToastContext';
+import { ToastProvider } from '@/context/ToastContext';
 import i18nConfig from '@/i18nConfig';
-import "../globals.css";
-
+import "../globals.scss";
 
 const GlobalFont = DotGothic16({
   subsets: ["latin"],
@@ -34,7 +33,7 @@ export default function RootLayout({
     <html lang={locale} dir={dir(locale)} style={{ scrollBehavior: 'smooth' }}>
       <body className={GlobalFont.className}>
         <ToastProvider>   
-        {children}
+          {children}
         </ToastProvider>
       </body>
     </html>
